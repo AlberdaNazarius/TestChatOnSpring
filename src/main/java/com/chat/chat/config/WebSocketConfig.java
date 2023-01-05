e496d43f9f32d9f3412ebb6760323408e562891f
AlberdaNazarius <103203230+AlberdaNazarius@users.noreply.github.com>
2023-01-05 21:54:55 +0200

    Fixed bug

--- src/main/java/com/chat/chat/config/WebSocketConfig.java
+++ src/main/java/com/chat/chat/config/WebSocketConfig.java
@@ -17,10 +17,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
-        registry.enableStompBrokerRelay("/topic")
-                .setRelayHost("localhost")
-                .setRelayPort(61613)
-                .setClientLogin("guest")
-                .setClientPasscode("guest");
+        registry.enableSimpleBroker("/topic");
     }
 }
